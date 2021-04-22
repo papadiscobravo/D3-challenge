6a407efe5ffb438d4ea06d606b420531494f3edd
pbernhardt <papadiscobravo@users.noreply.github.com>
2021-04-21 22:03:24 -0500

    data is showing up in console.log as it should

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,77 +1,43 @@
 console.log("app.js loaded");
+
+
+// initialize variables that let me easily change labels of columns of data and axes of graphs
+var stateLabel = "state";
+var abbrLabel = "abbreviation";
+var povertyLabel = "Percent of population who live in poverty";
+var ageLabel = "Median age of population";
+var incomeLabel = "Median income of population";
+var healthcareLabel = "Percent of population who lack heathcare coverage";
+var obesityLabel = "Percent of population who are obese";
+var smokingLabel = "Percent of population who smoke";
+
 //import data
 // data = import data.csv
+d3.csv("assets/data/data.csv").then(function(assignData) {
 
-data = [{
-    state: "state",
-    abbr: "AA",
-    poverty: 0.15,
-    age: 39.8,
-    income: 50000,
-    healthcare: .15,
-    obesity: 0.15,
-    smoking: 0.15,
-}
-];
-
-console.log("data.csv loaded");
-
-// read data into separate variable and 
-// declare variables for use in chart titles
-// poverty 4
-var state = data[1];
-var stateLabel = "state";
-console.log(`${stateLabel}: ${state}`);
-
-var abbr = data[2];
-var abbrLabel = "abbreviation";
-console.log(`${abbrLabel}: ${abbr}`);
-
-// poverty 4
-var poverty = data[3];
-var povertyLabel = "Percent of population who live in poverty";
-console.log(`${povertyLabel}: ${poverty}`);
-
-// age 6
-var age = data[5];
-var ageLabel = "Median age of population";
-console.log(`${ageLabel}: ${age}`);
-
-// income 8
-var income = data[7];
-var incomeLabel = "Median income of population";
-console.log(`${incomeLabel}: ${income}`);
-
-// healthcare 10
-var healthcare = data[9];
-var healthcareLabel = "Percent of population who lack heathcare coverage";
-console.log(`${healthcareLabel}: ${healthcare}`);
-
-// obesity 13
-var obesity = data[12];
-var obesityLabel = "Percent of population who are obese";
-console.log(`${obesityLabel}: ${obesity}`);
-
-// smoking 15
-var smoking = data[14];
-var smokingLabel = "Percent of population who smoke";
-console.log(`${smokingLabel}: ${smoking}`);
-
-
-
-d3.select("tbody")
-  .selectAll("tr")
-  .data(data)
-  .enter()
-  .append("tr")
-  .html(function(d) {
-    return `
-    <td>${d.state}</td>
-    <td>${d.poverty}</td>
-    <td>${d.age}</td>
-    <td>${d.income}</td>
-    <td>${d.healthcare}</td>
-    <td>${d.obesity}</td>
-    <td>${d.smoking}</td>
-    `;
+    console.log("data.csv loaded");
+    console.log(assignData);
+  
+    // log a list of names
+    var names = assignData.map(data => data.name);
+    console.log("names", names);
+  
+    // Cast each numerical value in assignData as a number
+    assignData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.age = +data.age;
+      data.income = +data.income;
+      data.healthcare = +data.healthcare;
+      data.obesity = +data.obesity;
+      data.smoking = +data.smokes;
+      console.log("-_-_-_-_-_-_-_-_-_-_-_-");
+      console.log(`${stateLabel}: ${data.state} (${data.abbr})`);
+      console.log(`${ageLabel}: ${data.age}`);
+      console.log(`${incomeLabel}: ${data.income}`);
+      console.log(`${healthcareLabel}: ${data.healthcare}`);
+      console.log(`${obesityLabel}: ${data.obesity}`);
+      console.log(`${smokingLabel}: ${data.smokes}`);
+    });
+  }).catch(function(error) {
+    console.log(error);
   });
